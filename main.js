d8087234a53e8cf4de8809d6fe72352212f55d0b
unknown <Aurpita-Das-Shormi>
2024-09-21 13:00:41 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+let fruits = ['apple','banana','cherry'];
+let fruits1 = fruits.toString();
+console.log(fruits1);
+
+
+let num = [10,20,30,40.50];
+let num1 = num.at(2);
+console.log(num1);
+
+let color = ['red','green','blue'];
+let color1 = color.join("-");
+console.log(color1);
+
+let arry =[1,2,3,4,5];
+ arry.pop();
+console.log(arry);
+
+
+let arry1 = ['apple','banana'];
+arry1.push('grape')
+console.log(arry1);
+
+let array2 = ['car','bike','bus'];
+array2.shift();
+console.log(array2);
+
+let arry3 = ['boat','ship'];
+arry3.push('train','plane');
+console.log(arry3);
+
